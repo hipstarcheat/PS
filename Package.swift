3920ccc3e0836ce96ebdd2bb0d1b484371268079
hipstarcheat <125921081+hipstarcheat@users.noreply.github.com>
2024-12-09 23:45:40 +0300

    Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,34 @@
-Package.swift
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "My App",  // Название вашего приложения
+    platforms: [
+        .iOS(.v14)  // Минимальная версия iOS
+    ],
+    products: [
+        .executableTarget(
+            name: "My App",
+            dependencies: [],
+            path: "Sources", // Укажите папку, где находятся исходные файлы
+            resources: [
+                .process("../Resources/Assets.xcassets") // Укажите путь к ресурсам
+            ]
+        ),
+    ],
+    dependencies: [
+        // Если нет сторонних библиотек, оставьте этот массив пустым
+        // Например, для Alamofire добавьте:
+        // .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "My App",
+            dependencies: [
+                // Здесь укажите зависимости для вашего приложения, если есть
+                // Например: "Alamofire"
+            ],
+            path: "./Sources"  // Укажите путь к исходным файлам вашего приложения
+        ),
+    ]
+)
